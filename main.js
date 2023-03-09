155643d80241e9d774a15c4332a698b6123abb4d
Juan Paz <pazju@javeriana.edu.co>
2023-03-09 12:49:46 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,3 @@
+import './src/blackjack'; // El navegador busca el index.js por defecto
+import './style.css';
+
---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,5 @@
+export {crearDeck} from './crear-deck';
+export {pedirCarta} from './pedir-carta';
+export {crearCartaHTML} from './crear-carta-html';
+export {turnoComputadora} from './turno-computadora';
+export {valorCarta} from './valor-carta';
